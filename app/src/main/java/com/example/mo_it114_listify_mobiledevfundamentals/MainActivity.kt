d480c039b35f2d3a4b141b19aafaa393309cedd3
zephyrstorm123 <107976319+zephyrstorm123@users.noreply.github.com>
2024-10-17 23:44:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mo_it114_listify_mobiledevfundamentals/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mo_it114_listify_mobiledevfundamentals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mo_it114_listify_mobiledevfundamentals.ui.theme.MOIT114LISTIFYMobileDevFundamentalsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MOIT114LISTIFYMobileDevFundamentalsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MOIT114LISTIFYMobileDevFundamentalsTheme {
+        Greeting("Android")
+    }
+}
