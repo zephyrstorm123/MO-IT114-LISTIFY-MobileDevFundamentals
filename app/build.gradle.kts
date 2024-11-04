de59aecccd60f76165f4cc9eca54e32f4df389fe
zephyrstorm123 <107976319+zephyrstorm123@users.noreply.github.com>
2024-11-04 16:22:58 +0800

    Fixed Register Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.3.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/mo_it114_listify_mobiledevfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/mo_it114_listify_mobiledevfundamentals/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mo_it114_listify_mobiledevfundamentals
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
+import android.content.Intent
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,8 +19,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createAccountBtn: Button = findViewById(R.id.createAcctBtn)
+
+        // Set an OnClickListener on the "Forgot Password" Button
+        createAccountBtn.setOnClickListener {
+            // Create an Intent to start the CreateAccountActivity
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
+
         }
     }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
